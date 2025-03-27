603c7e859855dd5c3ed55905bab4cc7dac50f6be
Vinay Gopinath <vinay@vinaygopinath.me>
2025-03-27 14:35:19 +0530

    Add default country code validator
    This adds a validator that checks whether the user-specified country code is a valid country code.
    Without using advanced checks, it simply validates whether the country code starts with a + and has at most 3 digits following the +.

---
+++ app/src/test/kotlin/org/vinaygopinath/launchchat/screens/settings/DefaultCountryCodeHelperTest.kt
@@ -0,0 +1,43 @@
+package org.vinaygopinath.launchchat.screens.settings
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.vinaygopinath.launchchat.screens.settings.DefaultCountryCodeHelper.isValidCountryCode
+
+class DefaultCountryCodeHelperTest {
+
+    @Test
+    fun `returns false when input is null`() {
+        assertThat(isValidCountryCode(null)).isFalse()
+    }
+
+    @Test
+    fun `returns true when input is a country code with no spaces`() {
+        assertThat(isValidCountryCode("+238")).isTrue()
+    }
+
+    @Test
+    fun `returns false when input is country code-like, but does not begin with a +`() {
+        assertThat(isValidCountryCode("254")).isFalse()
+    }
+
+    @Test
+    fun `returns false when input is country code-like with no spaces, but has more than 3 digits`() {
+        assertThat(isValidCountryCode("+38756")).isFalse()
+    }
+
+    @Test
+    fun `returns false when input is a country code but contains a leading space`() {
+        assertThat(isValidCountryCode(" +970")).isFalse()
+    }
+
+    @Test
+    fun `returns false when input is a country code but contains a trailing space`() {
+        assertThat(isValidCountryCode("+970 ")).isFalse()
+    }
+
+    @Test
+    fun `returns false when input is not a country code`() {
+        assertThat(isValidCountryCode("Hi!")).isFalse()
+    }
+}
