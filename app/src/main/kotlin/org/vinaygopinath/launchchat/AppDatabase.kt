65edc8153ee0076daac58ecebc9f5575ab363ea6
Vinay Gopinath <vinay@vinaygopinath.me>
2025-05-21 15:10:17 -0600

    Add conditional data migration to insert pre-populated chat apps

--- app/src/main/kotlin/org/vinaygopinath/launchchat/AppDatabase.kt
+++ app/src/main/kotlin/org/vinaygopinath/launchchat/AppDatabase.kt
@@ -1,6 +1,7 @@
 package org.vinaygopinath.launchchat
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -8,10 +9,13 @@ import androidx.room.TypeConverters
 import org.vinaygopinath.launchchat.converters.RoomTypeConverter
 import org.vinaygopinath.launchchat.daos.ActionDao
 import org.vinaygopinath.launchchat.daos.ActivityDao
+import org.vinaygopinath.launchchat.daos.ChatAppDao
 import org.vinaygopinath.launchchat.daos.DetailedActivityDao
+import org.vinaygopinath.launchchat.extensions.addConditionalDataMigration
 import org.vinaygopinath.launchchat.models.Action
 import org.vinaygopinath.launchchat.models.Activity
 import org.vinaygopinath.launchchat.models.ChatApp
+import org.vinaygopinath.launchchat.utils.DateUtils
 
 @Database(
     entities = [
@@ -19,21 +23,31 @@ import org.vinaygopinath.launchchat.models.ChatApp
         Action::class,
         ChatApp::class
     ],
-    version = 2
+    version = 2,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ]
 )
 @TypeConverters(RoomTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun activityDao(): ActivityDao
     abstract fun actionDao(): ActionDao
     abstract fun detailedActivityDao(): DetailedActivityDao
+    abstract fun chatAppDao(): ChatAppDao
 
     companion object {
-        fun buildDatabase(context: Context): AppDatabase {
+        fun buildDatabase(context: Context, dateUtils: DateUtils): AppDatabase {
             return Room.databaseBuilder(
                 context,
                 AppDatabase::class.java,
                 "launch-chat"
             ).build()
+                .addConditionalDataMigration(
+                    condition = { it.chatAppDao().getCount() == 0 },
+                    action = {
+                        it.chatAppDao().create(ChatApp.getPredefinedChatApps(dateUtils))
+                    }
+                )
         }
     }
 }
