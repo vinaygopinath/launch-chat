4b4829a9dff2e861324a11492e7e2b84a5926974
Lorenzo-d-L <lorenzodelaat@ziggo.nl>
2025-06-11 15:56:09 +0200

    Add delete methods to ActivityRepository for activities and associated actions

--- app/src/main/kotlin/org/vinaygopinath/launchchat/repositories/ActivityRepository.kt
+++ app/src/main/kotlin/org/vinaygopinath/launchchat/repositories/ActivityRepository.kt
@@ -1,12 +1,27 @@
 package org.vinaygopinath.launchchat.repositories
 
+import org.vinaygopinath.launchchat.daos.ActionDao
 import org.vinaygopinath.launchchat.daos.ActivityDao
 import org.vinaygopinath.launchchat.models.Activity
 import javax.inject.Inject
 
 class ActivityRepository @Inject constructor(
-    private val activityDao: ActivityDao
+    private val activityDao: ActivityDao,
+    private val actionDao: ActionDao
 ) {
+
+    suspend fun deleteActivitiesByIds(activityIds: Set<Long>) {
+        actionDao.deleteByActivityIds(activityIds.toList())
+    }
+
+    suspend fun deleteActionsByActivityIds(activityIds: Set<Long>) {
+        activityDao.deleteByIds(activityIds.toList())
+    }
+
+    suspend fun delete(activity: Activity) {
+        deleteActionsByActivityIds(setOf(activity.id))
+        activityDao.delete(activity.id)
+    }
 
     suspend fun create(activity: Activity): Activity {
         val newId = activityDao.create(activity)
