a7e80e8c7d97d55635473585c6d0233c21a6d21c
Lorenzo-d-L <lorenzodelaat@ziggo.nl>
2025-06-13 10:56:05 +0200

    Refactor HistoryAdapter by removing unused imports and cleaning up code

--- app/src/main/kotlin/org/vinaygopinath/launchchat/screens/history/HistoryAdapter.kt
+++ app/src/main/kotlin/org/vinaygopinath/launchchat/screens/history/HistoryAdapter.kt
@@ -3,10 +3,8 @@ package org.vinaygopinath.launchchat.screens.history
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.paging.PagingData
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textview.MaterialTextView
 import org.vinaygopinath.launchchat.R
