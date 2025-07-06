d38740d75fcab5d41a60ab8494e17650c7b63a10
Vinay Gopinath <vinay@vinaygopinath.me>
2025-07-06 11:31:56 -0600

    Update history list items to be selectable
    This updates the history list items (activities) to be selectable, showing a check icon next to the item when it is long-pressed.
    When one or more list items are selected, the toolbar shows a new "Delete" button, which, after a confirmation dialog, invokes the use case to irreversibly delete the selected items.

--- app/src/main/kotlin/org/vinaygopinath/launchchat/screens/history/HistoryViewModel.kt
+++ app/src/main/kotlin/org/vinaygopinath/launchchat/screens/history/HistoryViewModel.kt
@@ -4,16 +4,44 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.update
+import org.vinaygopinath.launchchat.screens.history.domain.DeleteSelectedActivitiesUseCase
 import org.vinaygopinath.launchchat.screens.history.domain.GetDetailedActivitiesUseCase
+import org.vinaygopinath.launchchat.utils.CoroutineUtil
+import org.vinaygopinath.launchchat.utils.DispatcherUtil
 import javax.inject.Inject
 
 @HiltViewModel
 class HistoryViewModel @Inject constructor(
-    useCase: GetDetailedActivitiesUseCase
+    private val dispatcherUtil: DispatcherUtil,
+    getDetailedActivitiesUseCase: GetDetailedActivitiesUseCase,
+    private val deleteSelectedActivitiesUseCase: DeleteSelectedActivitiesUseCase
 ) : ViewModel() {
 
-    val detailedActivities = useCase.execute()
+    private val internalUiState = MutableStateFlow<UiState>(UiState.None)
+    val uiState: StateFlow<UiState> = internalUiState.asStateFlow()
+
+    val detailedActivities = getDetailedActivitiesUseCase.execute()
         .distinctUntilChanged()
         .cachedIn(viewModelScope)
+
+    fun deleteSelectedActivitiesAndActions(activityIds: Set<Long>) {
+        CoroutineUtil.doWorkInBackground(
+            viewModelScope = viewModelScope,
+            dispatcherUtil = dispatcherUtil,
+            doWork = { deleteSelectedActivitiesUseCase.execute(activityIds) },
+            onComplete = {
+                internalUiState.update { UiState.DeleteSuccessful }
+            }
+        )
+    }
+
+    sealed class UiState {
+        data object None : UiState()
+        data object DeleteSuccessful : UiState()
+    }
 }
