ad77d0a3a7180e1e281b77ae933c3f0b152b077b
Vinay Gopinath <vinay@vinaygopinath.me>
2025-12-30 01:39:19 -0600

    Add getEnabledChatApps query to ChatAppDao

--- app/src/main/kotlin/org/vinaygopinath/launchchat/daos/ChatAppDao.kt
+++ app/src/main/kotlin/org/vinaygopinath/launchchat/daos/ChatAppDao.kt
@@ -3,6 +3,7 @@ package org.vinaygopinath.launchchat.daos
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import org.vinaygopinath.launchchat.models.ChatApp
 
 @Dao
@@ -10,6 +11,13 @@ interface ChatAppDao {
 
     @Query("SELECT COUNT(*) FROM " + ChatApp.TABLE_NAME)
     fun getCount(): Int
+
+    @Query(
+        "SELECT * FROM " + ChatApp.TABLE_NAME +
+            " WHERE is_enabled = 1 AND deleted_at IS NULL" +
+            " ORDER BY name ASC"
+    )
+    fun getEnabledChatApps(): Flow<List<ChatApp>>
 
     @Insert
     suspend fun create(chatApp: ChatApp): Long
