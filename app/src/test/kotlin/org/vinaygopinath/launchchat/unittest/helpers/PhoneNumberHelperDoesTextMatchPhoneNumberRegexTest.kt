72e6754f4995574af593b0e8fbf0a7326b91ffdd
Vinay Gopinath <vinay@vinaygopinath.me>
2025-12-30 01:35:39 -0600

    Remove TextHelper class
    Move doesTextMatchPhoneNumberRegex() to PhoneNumberHelper and update
    MainActivity to use PhoneNumberHelper directly. The TextHelper class
    is no longer needed as its functionality has been consolidated into
    PhoneNumberHelper and UsernameHelper.

--- app/src/test/kotlin/org/vinaygopinath/launchchat/unittest/helpers/PhoneNumberHelperDoesTextMatchPhoneNumberRegexTest.kt
+++ app/src/test/kotlin/org/vinaygopinath/launchchat/unittest/helpers/PhoneNumberHelperDoesTextMatchPhoneNumberRegexTest.kt
@@ -2,49 +2,55 @@ package org.vinaygopinath.launchchat.unittest.helpers
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
-import org.vinaygopinath.launchchat.helpers.TextHelper
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.kotlin.mock
+import org.vinaygopinath.launchchat.helpers.PhoneNumberHelper
 
-class TextHelperPasswordRegexTest {
+@RunWith(JUnit4::class)
+class PhoneNumberHelperDoesTextMatchPhoneNumberRegexTest {
+
+    private val helper = PhoneNumberHelper(mock())
 
     @Test
     fun `returns true when text is phone number with no formatting and no country code`() {
         val inputText = "111222333"
 
-        assertThat(TextHelper.doesTextMatchPhoneNumberRegex(inputText)).isTrue()
+        assertThat(helper.doesTextMatchPhoneNumberRegex(inputText)).isTrue()
     }
 
     @Test
     fun `returns true when text is phone number with no formatting and country code`() {
         val inputText = "+1111222333"
 
-        assertThat(TextHelper.doesTextMatchPhoneNumberRegex(inputText)).isTrue()
+        assertThat(helper.doesTextMatchPhoneNumberRegex(inputText)).isTrue()
     }
 
     @Test
     fun `returns true when text is phone number with space formatting`() {
         val inputText = "111 222 333"
 
-        assertThat(TextHelper.doesTextMatchPhoneNumberRegex(inputText)).isTrue()
+        assertThat(helper.doesTextMatchPhoneNumberRegex(inputText)).isTrue()
     }
 
     @Test
     fun `returns true when text is phone number with parentheses and space formatting`() {
         val inputText = "(111) 222 333"
 
-        assertThat(TextHelper.doesTextMatchPhoneNumberRegex(inputText)).isTrue()
+        assertThat(helper.doesTextMatchPhoneNumberRegex(inputText)).isTrue()
     }
 
     @Test
     fun `returns true when text is phone number with hyphen formatting`() {
         val inputText = "111-222-333"
 
-        assertThat(TextHelper.doesTextMatchPhoneNumberRegex(inputText)).isTrue()
+        assertThat(helper.doesTextMatchPhoneNumberRegex(inputText)).isTrue()
     }
 
     @Test
     fun `returns false when text includes non-numerical and non-formatting characters`() {
         val inputText = "Please contact us on this phone number: +1(111)-222 333"
 
-        assertThat(TextHelper.doesTextMatchPhoneNumberRegex(inputText)).isFalse()
+        assertThat(helper.doesTextMatchPhoneNumberRegex(inputText)).isFalse()
     }
 }
